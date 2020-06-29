d6ede2f14b9e28ca23b557dac81137493f2ffc7c
Roy Ho <roy.ho@glassesgallery.com>
2020-06-29 18:43:45 +0800

    execrises

--- code/node-app/app.js
+++ code/node-app/app.js
@@ -3,3 +3,7 @@ const fs = require('fs')
 var filename = 'notes.txt'
 fs.writeFileSync(filename, "test 2")
 fs.appendFileSync(filename, "\ntest ok")
+
+const getNotes = require('./futiles.js')
+const data = getNotes(filename)
+console.log(data)
--- code/node-app/app2.js
+++ code/node-app/app2.js
@@ -1,5 +1,5 @@
 const add = require('./utils.js')
 
-const sum = add(1,5)
+const sum = add(1,-5)
 
 console.log(sum)
