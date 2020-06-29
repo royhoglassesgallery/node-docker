6fcdd39827113070e7c616a78629d27410593545
Roy Ho <roy.ho@glassesgallery.com>
2020-06-29 18:17:10 +0800

    t3

---
+++ code/node-app/app2.js
@@ -0,0 +1,5 @@
+const add = require('./utils.js')
+
+const sum = add(1,5)
+
+console.log(sum)
---
+++ code/node-app/utils.js
@@ -0,0 +1,9 @@
+console.log('utils.js')
+
+const name = "Roy"
+
+const add = (a, b) => {
+    return a + b
+}
+
+module.exports = add
